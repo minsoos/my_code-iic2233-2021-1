d2f40a5a1c1ea66ccd267829db57d478442004b1
Min Soo Jeon <mjeon@uc.cl>
2021-03-25 15:47:53 -0300

    nada especial

--- Actividades/AF1/cargar.py
+++ Actividades/AF1/cargar.py
@@ -4,13 +4,13 @@ from collections import namedtuple, defaultdict
 def cargar_platos(ruta_archivo = "platos.csv"):
     archivo = open(ruta_archivo)
     lista_de_platos = archivo.readlines()
-    Plato = namedtuple("platos", ["categoria", "tiempo_preparacion", "precio", "ingredientes"])
+    Plato = namedtuple("platos", ["n", "categoria", "tiempo", "precio", "ingredientes"])
     diccionario_de_platos = dict()
     for i in range(len(lista_de_platos)):
         lista_de_platos[i] = lista_de_platos[i].strip().split(",")
-        plato = lista_de_platos[i]
-        tupla_aux = Plato(plato[1], int(plato[2]), int(plato[3]), set(plato[4:len(plato)]))
-        diccionario_de_platos[plato[0]] = tupla_aux
+        p = lista_de_platos[i]
+        tupla_aux = Plato(p[0], p[1], int(p[2]), int(p[3]), set(p[4:len(p)]))
+        diccionario_de_platos[p[0]] = tupla_aux
     return diccionario_de_platos
 
 
@@ -28,3 +28,7 @@ def cargar_ingredientes(ruta_archivo = "ingredientes.csv"):
 if __name__ == "__main__":
     # ================== PUEDES PROBAR TUS FUNCIONES AQUÍ =====================
     print(" PRUEBA CARGAR ".center(80, "="))
+    cargar_ingredientes()
+    platoscargados = cargar_platos()
+    for i in platoscargados:
+        print("ssd",platoscargados[i].categoria)
--- Actividades/AF1/consultas.py
+++ Actividades/AF1/consultas.py
@@ -1,13 +1,32 @@
-# Separar por los platos por categoria
+import cargar
+from collections import defaultdict
+platos = cargar.cargar_platos()
+print(platos)
 def platos_por_categoria(platos):
-    # COMPLETAR ESTA FUNCIÖN
-    pass
+    diccionario = defaultdict(list)
+    for i in platos:
+        diccionario[platos[i].categoria].append(platos[i].n)
+    return diccionario
+    ##############
+    categorias = set()
+    #for i in platos:
+       # categorias.add(i.categoria)
+    #for i in categorias:
+      #  for e in range(len(platos)):
+
 
 
 # Retorna los platos que no incluyan los ingredientes descartados
 def descartar_platos(ingredientes_descartados, platos):
-    # COMPLETAR ESTA FUNCIÖN
-    pass
+    for i in platos:
+        tiene_ingrediente_descartado = False
+        for e in ingredientes_descartados:
+            for h in platos[i].ingredientes:
+                if e == h:
+                    tiene_ingrediente_descartado = True
+        if tiene_ingrediente_descartado == True:
+            del platos[platos[i].n]
+    return platos
 
 
 # Recibe un plato, comprueba si hay ingredientes suficientes y los descuenta
@@ -25,3 +44,5 @@ def resumen_orden(lista_platos):
 if __name__ == "__main__":
     # ================== PUEDES PROBAR TUS FUNCIONES AQUÍ =====================
     print(" PRUEBA CONSULTAS ".center(80, "="))
+    platos_por_categoria(platos)
+    descartar_platos(ingredientes_descartados, platos)
