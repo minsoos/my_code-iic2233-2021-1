f3b7ff6106762dee0733bbdeaa0e653609640bd3
Min Soo Jeon <mjeon@uc.cl>
2021-04-15 15:24:20 -0400

    primer librero completado

--- Actividades/AF2/excepciones_estrellas.py
+++ Actividades/AF2/excepciones_estrellas.py
@@ -1,7 +1,7 @@
 class RutaPeligrosa:
-    # Completar
     def __init__(self, tipo_peligro, nombre_estrella):
-        pass
+        self.tipo_peligro = tipo_peligro
+        self.nombre_estrella = nombre_estrella
 
     def dar_alerta_peligro(self):
         pass
--- Actividades/AF2/verificar_estrellas.py
+++ Actividades/AF2/verificar_estrellas.py
@@ -2,31 +2,83 @@ from cargar_datos import cargar_estrellas, cargar_nombres_estrellas_cercanas
 
 
 def verificar_alias_estrella(estrella):
-    pass
+    alias = estrella.alias
+    letras_alias = alias[0:2]
+    numeros_alias = alias[2:4]
+    bool1 = numeros_alias.isdigit()
+    bool2 = letras_alias.isupper()
+    bool3 = "F" not in letras_alias
+    if bool1 and bool2 and bool3 == False:
+        raise ValueError("Error: El alias de la estrella es incorrecto.", bool1, bool2, bool3)
 
 
 def corregir_alias_estrella(estrella):
-    pass
+    try:
+        verificar_alias_estrella(estrella)
+    except ValueError as err:
+        print(err.args[0])
+        alias = estrella.alias
+        letras_alias = alias[0:2]
+        numeros_alias = alias[2:4]
+        if err.args[3] == False:
+            alias = alias.replace("F", "T")
+        if err.args[1] == False:
+            #Acá suponemos que están cambiados
+            alias = numeros_alias + letras_alias
+        ## Aquí vemos si después de los cambios está bien
+        letras_alias = alias[0:2]
+        numeros_alias = alias[2:4]
+        bool1 = numeros_alias.isdigit()
+        bool2 = letras_alias.isupper()
+        bool3 = "F" not in letras_alias
+        if bool1 and bool2 and bool3:
+            estrella.alias = alias
+            print(f"El alias de {estrella.nombre} fue correctamente corregido.\n")
 
 
 def verificar_distancia_estrella(estrella):
-    pass
+    if estrella.distancia < 0:
+        raise ValueError("Error: Distancia negativa.")
 
 
 def corregir_distancia_estrella(estrella):
-    pass
-
+    try:
+        verificar_distancia_estrella(estrella)
+    except ValueError as err:
+        print(err)
+        estrella.distancia = -estrella.distancia
+        print(f"La distancia de la estrella {estrella.nombre} fue corregida.\n")
 
 def verificar_magnitud_estrella(estrella):
-    pass
-
+    if not isinstance(estrella.magnitud, float):
+        raise TypeError("Error: Magnitud no es del tipo correcto.")
 
 def corregir_magnitud_estrella(estrella):
-    pass
+    try:
+        verificar_magnitud_estrella(estrella)
+    except TypeError as err:
+        print(err)
+        magnitud = estrella.magnitud
+        magnitud = magnitud.replace(";",".")
+        try:
+            estrella.magnitud = float(magnitud)
+            
+        except ValueError:
+            pass
+        else:
+            print(f"La magnitud de la estrella {estrella.nombre} fue corregida.\n")
+
+        
 
 
 def dar_alerta_estrella_cercana(nombre_estrella, diccionario_estrellas):
-    pass
+    try:
+        estrellita = diccionario_estrellas[nombre_estrella]
+        print(f"Estrella {nombre_estrella} está en nuestra base de datos.")
+        print(f"Su alias es {estrellita.alias}.")
+    except KeyError:
+        print(f"Estrella {nombre_estrella} NO está en nuestra base de datos.")
+        print("¡Alerta, puede ser una trampa de algún extraterrestre!")
 
 
 
@@ -37,12 +89,12 @@ if __name__ == "__main__":
     # Descomenta las funciones que quieras probar de la actividad
     print("Revisando posibles errores en las estrellas...\n")
     for estrella in diccionario_estrellas.values():
-        # corregir_alias_estrella(estrella)
-        # corregir_distancia_estrella(estrella)
-        # corregir_magnitud_estrella(estrella)
+        corregir_alias_estrella(estrella)
+        corregir_distancia_estrella(estrella)
+        corregir_magnitud_estrella(estrella)
         pass
 
     print("Revisando estrellas inexistentes...\n")
     for nombre_estrella in nombres_estrellas:
-        # dar_alerta_estrella_cercana(nombre_estrella, diccionario_estrellas)
+        dar_alerta_estrella_cercana(nombre_estrella, diccionario_estrellas)
         pass
